00ed6580b87f8f781ed5995cbe163a1bc925439f
Steve Taylor <steve@deployhub.com>
2021-12-08 15:42:07 -0700

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -164,7 +164,7 @@ if __name__ == '__main__':
 
     logging.info('Adding {}'.format(deb_file_path))
     os.system(
-        'reprepro -b {} --export=silent-never includedeb {} {}'.format(
+        'reprepro -b {} --keepunusednewfiles --export=silent-never includedeb {} {}'.format(
             apt_dir,
             deb_file_version,
             deb_file_path,
